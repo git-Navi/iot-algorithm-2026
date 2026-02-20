deb8e24331766bf398a142efbadb177eef34cf0d
git-Navi <122284636+git-Navi@users.noreply.github.com>
2026-02-20 17:25:39 +0900

    12일차 commit

---
+++ basic/algorithm01/app03.c/app03.c
@@ -0,0 +1,44 @@
+﻿// p52. 동적 할당
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int* x = malloc(sizeof(int));	// calloc(1, sizeof(int));
+	if (x == NULL) {
+		puts("메모리 할당 실패!");	// printf(); 사용 시에는 \n 사용해야함. 띄어쓰기 할 때.
+		return 1;
+	}
+
+	*x = 57;
+	printf("*x = %d\n", *x);
+
+	free(x);
+
+	// 배열 동적할당
+	int na;
+	printf("요소 개수 > ");
+	scanf("%d", &na);
+
+	int* ary = calloc(na, sizeof(int));	// malloc(sizeof(int) * na)
+	if (ary == NULL) {
+		puts("메모리 할당 실패!");
+		return 1;
+	}
+
+	printf("%d개의 정수를 입력하세요.\n", na);
+	for (int i = 0; i < na; i++) {
+		printf("a[%d]", i);
+		scanf("%d", &ary[i]);	// &ary[i]
+	}
+
+	printf("각 요소 값 출력 > \n");
+	for (int i = 0; i < na; i++) {
+		printf("a[%d] = %d\n", i, ary[i]);
+	}
+
+	free(ary);
+
+	return 0;
+}
---
+++ basic/algorithm01/app05/app05.c
@@ -0,0 +1,30 @@
+﻿// app05 - 소수 (Prime number)
+// 1000 이하의 소수 나열
+
+#include <stdio.h>
+
+int main(void) {
+	unsigned long counter = 0;	// 나눗셈 횟수
+	int prime_cnt = 0;
+
+	for (int n = 2; n <= 1000; n++) {
+		// int i 쓰면 지역 변수라서 이 코드 블럭이 끝나면 사라진다.
+		int i;
+		for (i = 2; i < n; i++) {
+			counter++;
+
+			if (n % i == 0) {	// n이 i로 나누어 떨어지면, 소수가 아님.
+				break;
+			}
+
+		}
+		if (n == i) {
+			prime_cnt += 1;
+			printf("%d\n", n);	// 소수값 출력
+		}
+	}
+	printf("1~1000 사이 소수 갯수 : %u\n", prime_cnt);
+	printf("나눗셈 실행 횟수 : %u\n", counter);
+
+	return 0;
+}
